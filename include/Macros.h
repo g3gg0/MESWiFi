8d8e1a9847824b12277c57cf5b33b3a4afbe12e9
g3gg0 <git@g3gg0.de>
2026-05-05 21:35:28 +0200

    fix macro

--- include/Macros.h
+++ include/Macros.h
@@ -3,7 +3,7 @@
 
 #define MIN(a, b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
 #define MAX(a, b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b; })
-#define COERCE(val, min, max) (MIN(max,MIN(min,val)))
+#define COERCE(val, min, max) (MAX(min,MIN(max,val)))
 
 #define xstr(s) str(s)
 #define str(s) #s
